41066a2d31ab6e9ecaa996b6beb0f30a58dd2803
anshumannehru <anshumannehru.steamtroops@gmail.com>
2022-11-16 23:53:36 +0530

    rest-framework

--- backend/base/views.py
+++ backend/base/views.py
@@ -1,13 +1,26 @@
 from django.shortcuts import render
 from django.http import JsonResponse
 from .fake import products
+from rest_framework.decorators import api_view
+from rest_framework.response import Response
 # Create your views here.
 # views will help in the writting of logics
 # these views will be regsitered in the urls.py file
 
 #This will have our base products file
+@api_view(['GET'])
 def getRoutes(request):
-    return JsonResponse('Hello',safe=False)
+    routes = [
+        '/api/products',
+        '/api/products/create/',
+        '/api/products/upload/',
+        '/api/products/<id>/reviews/',
+        '/api/products/top/',
+        '/api/products/<id>',
+        '/api/products/delete/<id>',
+        '/api/products/<update>/<id>'
+    ]
+    return Response(routes)
 
 def getProducts(request):
     return JsonResponse(products,safe=False)
